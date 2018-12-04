0cf6ab0558935650d7e39f797ddab606d1526f7c
harlankuo <37682407@qq.com>
2018-12-04 16:40:43 +0800

    Initial commit

---
+++ src/main/java/com/senyint/image/imageretrieverhttp/ImageRetrieverHttpApplication.java
@@ -0,0 +1,12 @@
+package com.senyint.image.imageretrieverhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRetrieverHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRetrieverHttpApplication.class, args);
+    }
+}
---
+++ src/main/java/com/senyint/image/imageretrieverhttp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.senyint.image.imageretrieverhttp.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
